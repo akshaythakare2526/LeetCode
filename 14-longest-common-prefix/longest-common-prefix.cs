c198f0e4c9a0957495b6d15cadf7bb1e1604f88f
Akshay Thakare <akshaythakare2526@gmail.com>
2026-09-03 23:59:28 +0530

    Time: 7 ms (15.12%) | Memory: 43.4 MB (32.74%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cs
+++ 14-longest-common-prefix/longest-common-prefix.cs
@@ -5,19 +5,40 @@ public class Solution
         if (strs == null || strs.Length == 0)
             return "";
 
-        string prefix = strs[0];
+        string prefix = "";
+        string prefix1 = "";
 
-        for (int i = 1; i < strs.Length; i++)
+        for (int i = 0; i < strs[0].Length; i++)
         {
-            while (!strs[i].StartsWith(prefix))
-            {
-                prefix = prefix.Substring(0, prefix.Length - 1);
+            prefix = prefix + strs[0][i];
 
-                if (prefix == "")
-                    return "";
+            int count = countPrefix(strs, prefix);
+
+            if (count == 0)
+            {
+                break;
+            }
+            else
+            {
+                prefix1 = prefix;
             }
         }
 
-        return prefix;
+        return prefix1;
+    }
+
+    public int countPrefix(string[] strs, string prefix)
+    {
+        int count = 0;
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].StartsWith(prefix))
+            {
+                count++;
+            }
+        }
+
+        return count == strs.Length ? count : 0;
     }
 }
